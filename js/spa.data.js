575215d8975d4c8a0a4700fdf2f336aab39d2377
jhkim <jhkim@pineone.com>
2019-07-25 16:04:13 +0900

    예제 5.5 데이터 모듈 생성 - spa/js/spa.data.js

---
+++ js/spa.data.js
@@ -0,0 +1,15 @@
+/*
+ * spa.data.js
+ * 데이터 모듈
+ */
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, spa */
+
+spa.data = (function () { return {}; }());
