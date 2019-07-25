53e7b175d1c4d26f0ccf277e34e8d691c42e9368
jhkim <jhkim@pineone.com>
2019-07-25 16:05:34 +0900

    예제 5.6 가짜 데이터 모듈 생성 - spa/js/spa.fake.js

---
+++ js/spa.fake.js
@@ -0,0 +1,15 @@
+/*
+ * spa.fake.js
+ * 가짜 모듈
+ */
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, spa */
+
+spa.fake = (function () { return {}; }());
