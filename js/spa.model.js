e69126f99a2634247b64874f3ce110e39d7966f8
jhkim <jhkim@pineone.com>
2019-08-01 09:47:46 +0900

    예제 6.5 모델에 메시징 기능 추가 - spa/js/spa.model.js

--- js/spa.model.js
+++ js/spa.model.js
@@ -73,6 +73,7 @@ spa.model = (function () {
   makeCid = function () {
     return 'c' + String( stateMap.cid_serial++);
   };
+
   clearPeopleDb = function () {
     var user = stateMap.user;
     stateMap.people_db = TAFFY();
@@ -82,6 +83,7 @@ spa.model = (function () {
       stateMap.people_cid_map[user.cid] = user;
     }
   };
+
   completeLogin = function (user_list) {
     var user_map = user_list[0];
     delete stateMap.people_cid_map[user_map.cid];
@@ -90,9 +92,10 @@ spa.model = (function () {
     stateMap.user.css_map = user_map.css_map;
     stateMap.people_cid_map[user_map._id] = stateMap.user;
 
-    // 채팅 기능을 추가할 때 여기서 채팅에 참여하게 해야 한다.
+    chat.join();
     $.gevent.publish('spa-login', [stateMap.user]);
   };
+
   makePerson = function (person_map) {
     var person,
       cid = person_map.cid,
@@ -162,8 +165,8 @@ spa.model = (function () {
 
     logout = function () {
       var is_removed, user = stateMap.user;
-      // 채팅 기능을 추가할 때 여기서 채팅방을 떠나야 한다.
 
+      chat._leave();
       is_removed = removePerson(user);
       stateMap.user = stateMap.anon_user;
 
@@ -229,13 +232,16 @@ spa.model = (function () {
   //
   chat = (function () {
     var
-      _publish_listchange,
-      _update_list, _leave_chat, join_chat;
+      _publish_listchange, _publish_updatechat,
+      _update_list, _leave_chat,
+      get_chatee, join_chat, send_msg, set_chatee,
+      chatee = null;
 
     // 내부 메서드 시작
     _update_list = function (arg_list) {
       var i, person_map, make_person_map,
-        people_list = arg_list[0];
+        people_list = arg_list[0],
+        is_chatee_online = false;
 
       clearPeopleDb();
 
@@ -257,23 +263,44 @@ spa.model = (function () {
           id: person_map._id,
           name: person_map.name
         };
+        if (chatee && chatee.id === make_person_map.id) {
+          is_chatee_online = true;
+        }
         makePerson(make_person_map);
       }
 
       stateMap.people_db.sort('name');
+
+      // 채팅 상대가 더는 온라인 상태가 아니면 채팅 상대 설정을 해제한다.
+      // 그럼 'spa-setchatee' 전역 이벤트가 일어난다.
+      if (chatee && !is_chatee_online) { set_chatee(''); }
     };
 
     _publish_listchange = function (arg_list) {
       _update_list(arg_list);
       $.gevent.publish('spa-listchange', [arg_list]);
     };
+
+    _publish_updatechat = function (arg_list) {
+      var msg_map = arg_list[0];
+
+      if (!chatee) { set_chatee(msg_map.sender_id); }
+      else if (msg_map.sender_id !== stateMap.user.id
+        && msg_map.sender_id !== chatee.id
+      ) { set_chatee(msg_map.sender_id); }
+
+      $.gevent.publish('spa-updatechat', [msg_map]);
+    };
     // 내부 메서드 끝
 
     _leave_chat = function () {
       var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+      chatee = null;
       stateMap.is_connected = false;
       if (sio) { sio.emit('leavechat'); }
     };
+
+    get_chatee = function() { return chatee; };
 
     join_chat = function () {
       var sio;
@@ -287,15 +314,58 @@ spa.model = (function () {
 
       sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
       sio.on('listchange', _publish_listchange);
+      sio.on('updatechat', _publish_updatechat);
       stateMap.is_connected = true;
+      return true;
+    };
+
+    send_msg = function (msg_text) {
+      var msg_map,
+        sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+      if (!sio) { return false; }
+      if (!(stateMap.user && chatee)) { return false; }
+
+      msg_map = {
+        dest_id: chatee.id,
+        dest_name: chatee.name,
+        sender_id: stateMap.user.id,
+        msg_text: msg_text
+      };
+
+      // 진행 중인 메시지를 볼 수 있게 updatechat를 발행
+      _publish_updatechat([msg_map]);
+      sio.emit('updatechat', msg_map);
+      return true;
+    };
+
+    set_chatee = function(person_id) {
+      var new_chatee;
+
+      new_chatee = stateMap.people_cid_map[person_id];
+
+      if (new_chatee) {
+        if (chatee && chatee.id === new_chatee.id) {
+          return false;
+        }
+      } else {
+        new_chatee = null;
+      }
+
+      $.gevent.publish('spa-setchatee',
+        {old_chatee: chatee, new_chatee: new_chatee }
+      );
+      chatee = new_chatee;
       return true;
     };
 
     return {
       _leave: _leave_chat,
-      join: join_chat
+      get_chatee: get_chatee,
+      join: join_chat,
+      send_msg: send_msg,
+      set_chatee: set_chatee
     };
-
   }());
 
   initModule = function () {
