8452a8dfb6a98f7eef873bd4b7f638bd4486247f
jhkim <jhkim@pineone.com>
2019-08-19 19:01:44 +0900

    예제 7.24 외부 라우트를 사용하기 위한 서버 애플리케이션 업데이트 - webapp/app.js

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ var
 
   app = express(),
   server = http.createServer(app);
-routes();
 // ------------ 모듈 스코프 변수 끝 ------------
 
 // ------------ 서버 설정 시작 ------------
@@ -43,40 +42,7 @@ app.configure('production', function() {
 });
 
 // 아래에 있는 모든 설정은 라우트 설정이다.
-app.get('/', function(request, response) {
-  response.redirect('/spa.html');
-});
-
-app.all('/:obj_type/*?', function (request, response, next) {
-  response.contentType('json');
-  next();
-});
-
-app.get('/:obj_type/list', function (request, response) {
-  response.send({title: request.params.obj_type + ' list'});
-});
-
-app.post('/:obj_type/create', function (request, response) {
-  response.send({title: request.params.obj_type + ' created'});
-});
-
-app.get('/:obj_type/read/:id([0-9]+)', function (request, response) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-  });
-});
-
-app.post('/:obj_type/update/:id([0-9]+)', function (request, response) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
-  });
-});
-
-app.get('/:obj_type/delete/:id([0-9]+)', function (request, response) {
-  response.send({
-    title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-  });
-});
+routes.configRoutes(app, server);
 // ------------ 서버 설정 끝 ------------
 
 // ------------ 서버 구동 시작 ------------
