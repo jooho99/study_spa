696931a0addd527dd5c91f4e4b110da3aefeadee
jhkim <jhkim@pineone.com>
2019-08-19 10:56:01 +0900

    예제 7.11 익스프레스를 이용한 서로 다른 환경 지원 - webapp/app.js

--- app.js
+++ app.js
@@ -22,10 +22,22 @@ var
 
 // ------------ 서버 설정 시작 ------------
 app.configure(function () {
-  app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
 });
+
+app.configure('development', function() {
+  app.use(express.logger());
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+
 app.get('/', function(request, response) {
   response.send('Hello Express');
 });
