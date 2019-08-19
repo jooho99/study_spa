892fdb82ff04262304e03ecc13121a4bfbc09760
jhkim <jhkim@pineone.com>
2019-08-19 18:34:48 +0900

    예제 7.20 완성된 범용 CRUD 라우트 - webapp/app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
- * app.js - 고급 라우팅을 지원하는 익스프레스 서버
+ * app.js - 범용 라우팅을 지원하는 익스프레스 서버
  */
 
 /*jslint         node    : true, continue : true,
@@ -45,34 +45,34 @@ app.get('/', function(request, response) {
   response.redirect('/spa.html');
 });
 
-app.all('/user/*?', function (request, response, next) {
+app.all('/:obj_type/*?', function (request, response, next) {
   response.contentType('json');
   next();
 });
 
-app.get('/user/list', function (request, response) {
-  response.send({title: 'user list'});
+app.get('/:obj_type/list', function (request, response) {
+  response.send({title: request.params.obj_type + ' list'});
 });
 
-app.post('/user/create', function (request, response) {
-  response.send({'title': 'user created'});
+app.post('/:obj_type/create', function (request, response) {
+  response.send({title: request.params.obj_type + ' created'});
 });
 
-app.get('/user/read/:id([0-9]+)', function (request, response) {
+app.get('/:obj_type/read/:id([0-9]+)', function (request, response) {
   response.send({
-    'title': 'user with id ' + request.params.id + ' found'
+    title: request.params.obj_type + ' with id ' + request.params.id + ' found'
   });
 });
 
-app.post('/user/update/:id([0-9]+)', function (request, response) {
+app.post('/:obj_type/update/:id([0-9]+)', function (request, response) {
   response.send({
-    'title': 'user with id ' + request.params.id + ' updated'
+    title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
   });
 });
 
-app.get('/user/delete/:id([0-9]+)', function (request, response) {
+app.get('/:obj_type/delete/:id([0-9]+)', function (request, response) {
   response.send({
-    title: 'user with id ' + request.params.id + ' deleted'
+    title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
   });
 });
 // ------------ 서버 설정 끝 ------------
