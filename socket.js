29862eb5ecaed62648f2c1ae925d2b7f27c3d512
jhkim <jhkim@pineone.com>
2019-08-22 20:13:00 +0900

    예제 7.31 파일 감시를 위한 서버 애플리케이션 업데이트 - webapp/socket.js

--- socket.js
+++ socket.js
@@ -1,5 +1,5 @@
 /*
- * socket.js - 간단한 socket.io 예제
+ * socket.js - 동적 JS 로딩 예제
  */
 
 /*jslint         node    : true, continue : true,
@@ -13,29 +13,54 @@
 // ------------ 모듈 스코프 변수 시작 ------------
 'use strict';
 var
-  countUp,
+  setWatch,
 
   http = require('http'),
   express = require('express'),
   socketIo = require('socket.io'),
+  fsHandle = require('fs'),
 
   app = express(),
   server = http.createServer(app),
   io = socketIo.listen(server),
-  countIdx = 0
+  watchMap = {}
 ;
 // ------------ 모듈 스코프 변수 끝 ------------
 
 // ------------ 유틸리티 메서드 시작 ------------
-countUp = function () {
-  countIdx++;
-  console.log(countIdx);
-  io.sockets.send(countIdx);
+setWatch = function (url_path, file_type) {
+  console.log('setWatch called on ' + url_path);
+  if (!watchMap[url_path]) {
+    console.log('setting watch on ' + url_path);
+
+    fsHandle.watchFile(
+      url_path.split('?')[0].slice(1),
+      function (current, previous) {
+        console.log('previous ', previous);
+        console.log('current ', current);
+        console.log('file accessed');
+        if (current.mtime !== previous.mtime) {
+          console.log('file changed');
+          io.sockets.emit(file_type, url_path);
+        }
+      }
+    );
+    watchMap[url_path] = true;
+  }
 };
 // ------------ 유틸리티 메서드 끝 ------------
 
 // ------------ 서버 설정 시작 ------------
 app.configure(function() {
+  app.use(function (request, response, next) {
+    console.log(request.url);
+    if (request.url.indexOf('/js/') >= 0) {
+      setWatch(request.url, 'script');
+    } else if (request.url.indexOf('/css/') >= 0) {
+      setWatch(request.url, 'stylesheet');
+    }
+    next();
+  });
   app.use(express.static(__dirname + '/'));
 });
 
@@ -50,6 +75,4 @@ console.log(
   'Express server listening on port %d in %s mode',
   server.address().port, app.settings.env
 );
-
-setInterval(countUp, 1000);
 // ------------ 서버 구동 끝 ------------
